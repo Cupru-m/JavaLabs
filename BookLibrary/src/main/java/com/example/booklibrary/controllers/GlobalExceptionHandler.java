01eb215161b6202d50433b01b955b81d3a3de5f3
unknown <kuprum321@gmail.com>
2024-03-12 11:03:46 +0300

    Update GlobalExceptionHandler.java

--- BookLibrary/src/main/java/com/example/booklibrary/controllers/GlobalExceptionHandler.java
+++ BookLibrary/src/main/java/com/example/booklibrary/controllers/GlobalExceptionHandler.java
@@ -11,10 +11,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<String> handleNoHandlerFoundException(NoHandlerFoundException ex) {
-        return new ResponseEntity<>("Bad Request: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+    
     @ExceptionHandler(BookListEmptyException.class)
     public ResponseEntity<String> handleBookListEmptyException(BookListEmptyException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
